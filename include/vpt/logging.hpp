2eb661285f3be0b5e08ad357cc3753736a6defd1
Shibodd <simonebondi456@gmail.com>
2025-08-03 22:01:06 +0200

    Logging: add debug

--- include/vpt/logging.hpp
+++ include/vpt/logging.hpp
@@ -4,12 +4,22 @@
 #include <source_location>
 #include <iostream>
 
+namespace logging {
+namespace detail {
+
+inline static std::ostream& write_loc_info(std::ostream& os, std::source_location loc = std::source_location::current()) {
+  return os << loc.file_name() << '(' << loc.line() << ':' << loc.column() << ") `" << loc.function_name() << "`";
+}
+} // namespace detail
+} // namespace logging
+
 #define vptFATAL(x) do { std::clog << "[FATAL]: " << x << "\n"; exit(1); } while(false)
 #define vptWARN(x) do { std::clog << "[WARN]: " << x << "\n"; } while(false)
 #define vptINFO(x) do { std::clog << "[INFO]: " << x << "\n"; } while(false)
 #define vptDEBUG(x) do { \
-  auto loc = std::source_location::current; \
-  std::clog << "[DBG] (" << loc.file_name() << '(' << loc.line() << ':' << loc.column() << ") `" << loc.function_name() << "`" << "): " << x << std::endl; \
+  std::clog << "[DBG] ("; \
+  logging::detail::write_loc_info(std::clog); \
+  std::clog << "): " << x << std::endl; \
 } while(false)
 
 #endif // !VPT_LOGGING_HPP
