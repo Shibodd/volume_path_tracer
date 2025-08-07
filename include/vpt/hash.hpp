ebde9a78f7645c5cb75a481b5cf6d677be0e4936
Shibodd <simonebondi456@gmail.com>
2025-08-07 16:39:29 +0200

    Add pcg-cpp and RNG facilities

---
+++ include/vpt/hash.hpp
@@ -0,0 +1,64 @@
+#ifndef VPT_HASH_HPP
+#define VPT_HASH_HPP
+
+#include <bit>
+#include <cstdint>
+#include <cstddef>
+
+#include <vpt/utils.hpp>
+
+namespace vpt {
+namespace detail {
+
+static inline uint64_t MurmurHash64A_padded ( const void * key, int len, uint64_t seed )
+{
+  constexpr uint64_t m = 0xc6a4a7935bd1e995;
+  constexpr int r = 47;
+
+  uint64_t h = seed ^ (len * m);
+
+  const uint64_t * data = (const uint64_t *)key;
+  const uint64_t * end = data + (len/8);
+
+  while(data != end)
+  {
+    static_assert(std::endian::little == std::endian::native);
+    uint64_t k = *data++;
+
+    k *= m; 
+    k ^= k >> r; 
+    k *= m; 
+    
+    h ^= k;
+    h *= m; 
+  }
+
+  h ^= h >> r;
+  h *= m;
+  h ^= h >> r;
+
+  return h;
+}
+
+}
+
+template <typename... Args>
+static inline uint64_t hash(uint64_t seed, Args... args) {
+  constexpr size_t args_size = (sizeof(Args) + ...);
+
+  constexpr size_t size = ceildiv<size_t>(args_size, 8) * 8;
+  uint64_t key[size];
+  key[size-1] = 0;
+
+  char* data = reinterpret_cast<char*>(key);
+  ([&](auto arg) {
+    memcpy(data, &arg, sizeof(arg));
+    data += sizeof(arg);
+  }(args), ...);
+
+  return MurmurHash64A_padded(key, size, seed);
+}
+
+} // namespace vpt
+
+#endif // !VPT_HASH_HPP
---
+++ include/vpt/random.hpp
@@ -0,0 +1,43 @@
+#ifndef VPT_RANDOM_HPP
+#define VPT_RANDOM_HPP
+
+#include <pcg/pcg_random.hpp>
+#include <vpt/hash.hpp>
+
+namespace vpt {
+
+struct RandomNumberGenerator {
+  using engine = pcg32_fast;
+
+  RandomNumberGenerator(engine::result_type seed)
+    : m_seed(seed)
+  { }
+
+  void begin_job(size_t jid) {
+    m_engine.seed(hash(m_seed, jid));
+  }
+
+  template<typename T>
+  T uniform();
+
+  template <>
+  uint32_t uniform() {
+    auto val = m_engine();
+    static_assert(std::is_same_v<decltype(val), uint32_t>);
+    return val;
+  }
+
+  template <>
+  float uniform() {
+    constexpr float ONE_MINUS_EPS = float(0x1.fffffep-1);
+    return std::min<float>(ONE_MINUS_EPS, uniform<uint32_t>() * 0x1p-32f);
+  }
+
+private:
+  engine m_engine;
+  engine::result_type m_seed;
+};
+
+} // namespace vpt
+
+#endif // !VPT_RANDOM_HPP
