2b1b7096fbd5de044a4d342ace67c2e3a6471e77
Shibodd <simonebondi456@gmail.com>
2025-08-12 08:44:17 +0200

    Add Camera

---
+++ include/vpt/camera.hpp
@@ -0,0 +1,37 @@
+#ifndef VPT_CAMERA_HPP
+#define VPT_CAMERA_HPP
+
+#include <Eigen/Dense>
+#include <vpt/image.hpp>
+#include <vpt/configuration.hpp>
+#include <vpt/ray.hpp>
+
+namespace vpt {
+
+struct Camera {
+  Camera(const CameraParameters& p, const image_size_t& im_sz);
+
+  inline Ray generate_ray(const image_point_t& raster) const {
+    Eigen::Vector3f raster_coords {
+      raster.cast<float>().x() + 0.5f,
+      raster.cast<float>().y() + 0.5f,
+      0.0f
+    };
+    
+    return {
+      params().position,
+      (m_raster_to_world_dir * raster_coords).normalized()
+    };
+  }
+
+  const CameraParameters& params() const { return m_params; }
+  const Eigen::Affine3f& raster_to_world_dir() const { return m_raster_to_world_dir; }
+private:
+  CameraParameters m_params;
+  Eigen::Affine3f m_raster_to_world_dir;
+};
+
+
+} // namespace vpt
+
+#endif // !VPT_CAMERA_HPP
---
+++ src/camera.cpp
@@ -0,0 +1,58 @@
+#include <vpt/camera.hpp>
+
+namespace vpt {
+
+static inline Eigen::Affine3f camera_to_world(const Eigen::Vector3f& pos, const Eigen::Vector3f& look, const Eigen::Vector3f& up) {
+  Eigen::Vector3f dir = (look - pos).normalized();
+  Eigen::Vector3f left = up.normalized().cross(dir);
+  Eigen::Vector3f new_up = dir.cross(left);
+
+  Eigen::Affine3f tf;
+  tf.setIdentity();
+  tf.linear().col(0) = left;
+  tf.linear().col(1) = new_up;
+  tf.linear().col(2) = dir;
+  tf.translation() = pos;
+
+  return tf;
+}
+
+/* Raster [0,res.w,0]x[0,res.h,0] to screen [-1,1,0]x[-1,1,0] */
+static inline Eigen::Affine3f raster_to_screen(const image_size_t& bounds) {
+  Eigen::Vector3f half_bounds { static_cast<float>(bounds.x()) / 2.0f, static_cast<float>(bounds.y()) / 2.0f, 0.0f };
+
+  Eigen::Affine3f tf;
+  tf.setIdentity();
+  tf.linear() = (-half_bounds.cwiseInverse()).asDiagonal();
+  tf.linear().bottomRightCorner<1,1>()[0] = 0.0f;
+  tf.translation() = Eigen::Vector3f { 1.0f, 1.0f, 0.0f };
+
+  return tf;
+}
+
+/* Normalized film [-1,1,0]x[-1,1,0] to camera assuming film is on the near plane, with near plane distance = 1 */ 
+static inline Eigen::Affine3f screen_to_camera(float vfov, float ar) {
+  float tan_vfov2 = std::tan(vfov/2);
+
+  Eigen::Affine3f tf;
+  tf.setIdentity();
+  tf.linear() = Eigen::Vector3f { ar * tan_vfov2, tan_vfov2, 0.0f }.asDiagonal();
+  tf.translation() = Eigen::Vector3f { 0.0f, 0.0f, 1.0f };
+
+  return tf;
+}
+
+Camera::Camera(const CameraParameters& p, const image_size_t& im_sz)
+  : m_params(p)
+{
+  float ar = static_cast<float>(im_sz.x()) / static_cast<float>(im_sz.y());
+  float vfov_rad = std::numbers::pi * p.vfov_deg / 180.0f;
+
+  auto c2w = camera_to_world(p.position, p.look, p.up);
+  auto s2c = screen_to_camera(vfov_rad, ar);
+  auto r2s = raster_to_screen(im_sz);
+
+  m_raster_to_world_dir = c2w.linear() * s2c * r2s;
+}
+
+} // namespace vpt
