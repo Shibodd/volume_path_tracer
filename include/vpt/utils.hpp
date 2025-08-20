a18b3f76944b33a33d62e99d17a2c137522622e0
Shibodd <simonebondi456@gmail.com>
2025-08-20 15:59:28 +0200

    Add csv utils

--- include/vpt/utils.hpp
+++ include/vpt/utils.hpp
@@ -2,6 +2,7 @@
 #define VPT_UTILS_HPP
 
 #include <type_traits>
+#include <iostream>
 
 namespace vpt {
 
@@ -9,6 +10,16 @@ template <typename T>
 requires std::is_integral_v<T>
 constexpr static inline T ceildiv(T x, T y) {
   return x / y + (x % y != 0);
+}
+
+template <typename T>
+static inline std::ostream& print_csv(std::ostream& os, const T& arg) {
+  return os << arg;
+}
+
+template <typename T, typename... Args>
+static inline std::ostream& print_csv(std::ostream& os, const T& first, const Args&... rest) {
+  return print_csv(os << first << ",", rest...);
 }
 
 } // namespace vpt
