d4c45f6237ed19adefd86781e3f22405720528b3
Shibodd <simonebondi456@gmail.com>
2025-08-21 09:32:28 +0200

    Volume Grids: allow not having temperature

--- include/vpt/volume_grids.hpp
+++ include/vpt/volume_grids.hpp
@@ -2,6 +2,7 @@
 #define VPT_VOLUMEGRIDS_HPP
 
 #include <filesystem>
+#include <optional>
 
 #include <nanovdb/GridHandle.h>
 
@@ -11,17 +12,19 @@ struct VolumeGrids {
   using GridHandleT = nanovdb::GridHandle<nanovdb::HostBuffer>;
   using GridT = nanovdb::NanoGrid<float>;
 
+  VolumeGrids(GridHandleT&& density);
   VolumeGrids(GridHandleT&& density, GridHandleT&& temperature);
 
   inline GridT& density() const { return *m_density; }
   inline GridT& temperature() const { return *m_temperature; }
+  inline bool has_temperature() const { return m_temperature != nullptr; }
 
   static VolumeGrids read_from_file(const std::filesystem::path& path);
   static VolumeGrids generate_donut();
 
 private:
   GridHandleT m_density_handle;
-  GridHandleT m_temperature_handle;
+  std::optional<GridHandleT> m_temperature_handle;
 
   GridT* m_density;
   GridT* m_temperature;
--- src/volume_grids.cpp
+++ src/volume_grids.cpp
@@ -1,4 +1,5 @@
 #include <utility>
+#include <optional>
 
 // ffs... build your shit with warnings enabled pls
 #pragma GCC diagnostic push
@@ -14,46 +15,52 @@
 
 namespace vpt {
 
+VolumeGrids::VolumeGrids(GridHandleT&& h_density)
+  : m_density_handle(std::forward<GridHandleT>(h_density)),
+    m_temperature_handle(std::nullopt),
+    m_density(m_density_handle.grid<float>()),
+    m_temperature(nullptr)
+{}
+
 VolumeGrids::VolumeGrids(GridHandleT&& h_density, GridHandleT&& h_temperature)
   : m_density_handle(std::forward<GridHandleT>(h_density)),
     m_temperature_handle(std::forward<GridHandleT>(h_temperature)),
     m_density(m_density_handle.grid<float>()),
-    m_temperature(m_temperature_handle.grid<float>())
+    m_temperature(m_temperature_handle->grid<float>())
 {}
 
 VolumeGrids VolumeGrids::generate_donut() {
-  nanovdb::tools::build::FloatGrid temperature_grid(0.0f); // cold-ass donut
-  
-  return VolumeGrids {
-    nanovdb::tools::createFogVolumeTorus(),
-    nanovdb::tools::createNanoGrid(temperature_grid)
-  };
+  return VolumeGrids { nanovdb::tools::createFogVolumeTorus() };
+}
+
+static inline std::optional<VolumeGrids::GridHandleT> nanovdb_try_read_grid(const std::filesystem::path& path, const std::string& grid_name) {
+  try {
+    return nanovdb::io::readGrid<VolumeGrids::GridHandleT::BufferType>(path, grid_name);
+  } catch (const std::runtime_error& e) {
+    vptWARN("NanoVDB failed to read grid \"" << grid_name << "\" from file \"" << path << "\": " << e.what());
+  }
+  return std::nullopt;
 }
 
 static inline VolumeGrids::GridHandleT nanovdb_read_grid_or_die(const std::filesystem::path& path, const std::string& grid_name) {
-  constexpr auto try_read = [](const std::filesystem::path& path, const std::string& grid_name) {
-    try {
-      return nanovdb::io::readGrid<VolumeGrids::GridHandleT::BufferType>(path, grid_name);
-    } catch (const std::runtime_error& e) {
-      vptFATAL("NanoVDB failed to read grid \"" << grid_name << "\" from file \"" << path << "\": " << e.what());
-    }
-    std::unreachable();
-  };
-
-  auto grid = try_read(path, grid_name);
+  auto grid = nanovdb_try_read_grid(path, grid_name);
   
   if (not grid) {
     vptFATAL("NanoVDB file " << path << " does not contain the \"" << grid_name << "\" grid.");
   }
 
-  return grid;
+  return std::move(*grid);
 }
 
 VolumeGrids VolumeGrids::read_from_file(const std::filesystem::path& path) {
-  return VolumeGrids {
-    nanovdb_read_grid_or_die(path, "temperature"),
-    nanovdb_read_grid_or_die(path, "density")
-  };
+  GridHandleT density = nanovdb_read_grid_or_die(path, "density");
+  std::optional<GridHandleT> temperature = nanovdb_try_read_grid(path, "temperature");
+
+  if (temperature) {
+    return VolumeGrids { std::move(density), std::move(*temperature) };
+  }
+
+  return VolumeGrids { std::move(density) };
 }
 
 } // namespace vpt
