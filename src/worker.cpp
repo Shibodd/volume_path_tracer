55ed0f27edaf26f05bc2ccd16c94102e3fea3670
Shibodd <simonebondi456@gmail.com>
2025-09-05 13:34:02 +0200

    Worker: Remove outdated comments

--- src/worker.cpp
+++ src/worker.cpp
@@ -118,19 +118,11 @@ void run(const WorkerParameters& params, const Volume& vol, const Camera& camera
           // std::this_thread::sleep_for(std::chrono::seconds(1));
         }
 
-        /*
-          Based on PBRT's SimpleVolPathIntegrator
-          https://github.com/mmp/pbrt-v4
-        */
-
         Eigen::Vector2f jitter { rng.uniform<float>(), rng.uniform<float>() };
         jitter *= params.use_jitter? 0.5 : 0.0;
 
         Ray r = camera.generate_ray(pt, jitter);
         logger.new_ray(r);
-
-        // vol.log_dda_trace(r, density_acc);
-        // vol.log_majorant_trace(r, density_acc);
 
         Eigen::Vector3f L = decltype(L)::Zero();
         bool terminated = false;
